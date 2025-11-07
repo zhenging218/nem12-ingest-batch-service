2cdb93e22546b4f46ca96c5bc42d1aea2b40225a
Yiu Zhen Ging <zhenging218@gmail.com>
2025-11-07 22:04:54 +0800

    initial commit

---
+++ src/main/java/com/yiuzg/flo/nem12/ingest/Nem12IngestBatchServiceApplication.java
@@ -0,0 +1,14 @@
+package com.yiuzg.flo.nem12.ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nem12IngestBatchServiceApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nem12IngestBatchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yiuzg/flo/nem12/ingest/Nem12IngestBatchServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yiuzg.flo.nem12.ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nem12IngestBatchServiceApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
